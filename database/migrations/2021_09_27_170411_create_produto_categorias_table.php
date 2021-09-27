e16bbebbe8979e0ae8bd70282fda2e0885f6d066
amandasilvaf <amandasilvaf1995@gmail.com>
2021-09-27 14:32:19 -0300

    Cria Models e Migrations
    Produto, Categoria, ProdutoCategoria

---
+++ database/migrations/2021_09_27_170411_create_produto_categorias_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProdutoCategoriasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produto_categorias', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produto_categorias');
+    }
+}
